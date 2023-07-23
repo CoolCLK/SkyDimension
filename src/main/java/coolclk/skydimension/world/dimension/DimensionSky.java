934568f3bcc69b476cf3d6f2ffa8158133354897
CoolCLK <mr.chenlk123@qq.com>
2023-07-23 10:44:15 +0800

    Update.

--- src/main/java/coolclk/skydimension/world/dimension/DimensionSky.java
+++ src/main/java/coolclk/skydimension/world/dimension/DimensionSky.java
@@ -3,8 +3,6 @@ package coolclk.skydimension.world.dimension;
 import coolclk.skydimension.world.provider.WorldProviderSky;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.DimensionType;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
 
 import static coolclk.skydimension.SkyDimension.LOGGER;
 
@@ -13,8 +11,6 @@ public class DimensionSky {
     private final static String dimensionName = "sky";
     private final static String dimensionSuffix = "_" + dimensionName;
     private final static DimensionType dimensionType = DimensionType.register(DimensionSky.getName(), DimensionSky.getSuffix(), DimensionSky.getId(), WorldProviderSky.class, false);
-    private final static WorldProvider dimensionWorldProvider = dimensionType.createDimension();
-    private final static World dimensionWorld = ((WorldProviderSky) dimensionWorldProvider).getWorld();
     public static int getId() {
         return dimensionId;
     }
@@ -31,13 +27,9 @@ public class DimensionSky {
         return dimensionSuffix;
     }
 
-    public static World getWorld() {
-        return dimensionWorld;
-    }
-
     public static void go(EntityPlayer player) {
-        if (player.getEntityWorld() != DimensionSky.getWorld()) {
-            player.setWorld(DimensionSky.getWorld());
+        if (player.dimension != DimensionSky.getId()) {
+            player.changeDimension(DimensionSky.getId());
             LOGGER.debug("Sending player to dimension.");
         }
     }
