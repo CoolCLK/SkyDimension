633da4c2bfed798e039303a2f5dc3e36afe8d4c7
CoolCLK <mr.chenlk123@qq.com>
2023-07-24 21:20:44 +0800

    Update.

--- src/main/java/coolclk/skydimension/event/PlayerEvent.java
+++ src/main/java/coolclk/skydimension/event/PlayerEvent.java
@@ -4,12 +4,12 @@ import coolclk.skydimension.SkyDimension;
 import coolclk.skydimension.world.dimension.DimensionSky;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Random;
 
-@Mod.EventBusSubscriber(modid = SkyDimension.MOD_ID)
+@EventBusSubscriber(modid = SkyDimension.MOD_ID)
 public class PlayerEvent {
     @SubscribeEvent
     public static void onPlayerWakeUpEvent(PlayerWakeUpEvent event) {
--- src/main/java/coolclk/skydimension/event/WorldEvent.java
+++ src/main/java/coolclk/skydimension/event/WorldEvent.java
@@ -2,10 +2,10 @@ package coolclk.skydimension.event;
 
 import coolclk.skydimension.SkyDimension;
 import net.minecraftforge.event.terraingen.OreGenEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber(modid = SkyDimension.MOD_ID)
+@EventBusSubscriber(modid = SkyDimension.MOD_ID)
 public class WorldEvent {
     @SubscribeEvent
     public static void afterOreGenEvent(OreGenEvent.Post event) {
