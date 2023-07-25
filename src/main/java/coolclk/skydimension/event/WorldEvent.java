46275547dfd4f03f196ff3b4860d5be03a7abd13
CoolCLK <mr.chenlk123@qq.com>
2023-07-25 18:35:48 +0800

    Update.

--- src/main/java/coolclk/skydimension/event/WorldEvent.java
+++ src/main/java/coolclk/skydimension/event/WorldEvent.java
@@ -2,18 +2,22 @@ package coolclk.skydimension.event;
 
 import coolclk.skydimension.SkyDimension;
 import coolclk.skydimension.world.dimension.DimensionSky;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.DimensionType;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @EventBusSubscriber(modid = SkyDimension.MOD_ID)
 public class WorldEvent {
     @SubscribeEvent
-    public static void onWorldTick(TickEvent.WorldTickEvent event) {
-        event.world.getLoadedEntityList().stream().filter(e -> e.dimension == DimensionSky.getDimensionId() && e.getPosition().getY() <= 0).findAny().ifPresent(e -> e.changeDimension(DimensionType.OVERWORLD.getId(), (world, entity, yaw) -> {
-            entity.fallDistance = 0;
-            entity.setPosition(world.getSpawnPoint().getX(), world.getTopSolidOrLiquidBlock(world.getSpawnPoint()).getY(), world.getSpawnPoint().getZ());
-        }));
+    public static void onWorldTick(LivingFallEvent event) {
+        Entity fallEntity = event.getEntity();
+        if (fallEntity.getPosition().getY() <= 0 && fallEntity.dimension == DimensionSky.getDimensionId()) {
+            fallEntity.changeDimension(DimensionType.OVERWORLD.getId(), (world, entity, yaw) -> {
+                entity.fallDistance = 0;
+                entity.setPosition(world.getSpawnPoint().getX(), world.getTopSolidOrLiquidBlock(world.getSpawnPoint()).getY(), world.getSpawnPoint().getZ());
+            });
+        }
     }
 }
